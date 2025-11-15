5ae672020b2d841621154a9948766bc3f4d92e75
soash <soash2002@gmail.com>
2025-11-16 01:52:17 +0600

    commit

--- training/views.py
+++ training/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Training
 
 def training_list(request):
-    trainings = Training.objects.filter(is_active=True)
+    trainings = Training.objects.filter(is_active=True, medium='offline')
+    return render(request, 'training/training_list.html', {'trainings': trainings})
+
+def online_training_list(request):
+    trainings = Training.objects.filter(is_active=True, medium='online')
     return render(request, 'training/training_list.html', {'trainings': trainings})
 
 def training_detail(request, slug):
     training = get_object_or_404(Training, slug=slug)
     return render(request, 'training/training_detail.html', {'training': training})
+
