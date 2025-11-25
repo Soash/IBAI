f01efd2b57268364ce8a7bbc5dbd67637acc9e07
soash <soash2002@gmail.com>
2025-11-25 19:30:50 +0600

    commit

--- core/views.py
+++ core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from courses.models import Course, Category
+from training.models import Training
 
 def home(request):
     courses = Course.objects.all()
     categories = Category.objects.all()
+    trainings = Training.objects.filter(is_training=True)
     context = {
         'courses': courses,
-        'categories': categories
+        'categories': categories,
+        'trainings': trainings
     }
     return render(request, 'core/home.html', context)
 
