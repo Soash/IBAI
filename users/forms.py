0bf7c777210d88bcc761556629848cde01c3a99e
soash <soash2002@gmail.com>
2025-12-22 23:20:38 +0600

    commit

--- users/forms.py
+++ users/forms.py
@@ -10,11 +10,19 @@ class UserRegistrationForm(forms.ModelForm):
         fields = [
             'first_name', 'email', 'phone_number', 'user_type',
             'designation', 'university_name', 'session',
-            'bio', 'profile_picture', 'facebook_link', 'linkedin_link', 'website'
+            'bio', 'profile_picture', 'facebook_link', 'linkedin_link'
         ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
+        self.fields['first_name'].required = True
+        self.fields['email'].required = True
+        self.fields['phone_number'].required = True
+        self.fields['user_type'].required = True
+        self.fields['designation'].required = True
+        self.fields['university_name'].required = True
+        
 
         # Add CSS classes to all fields
         for field in self.fields.values():
